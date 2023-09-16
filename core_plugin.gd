645292ca8e315bc1461d146e9790d32f2338546d
Charlie Whitfield <charliewhitfield@zoho.com>
2023-09-16 18:41:48 -0400

    Change autoload format in config files

--- core_plugin.gd
+++ core_plugin.gd
@@ -27,25 +27,25 @@ extends EditorPlugin
 # Project Settings.
 
 var _base_cfg: ConfigFile # res://addons/ivoyager_core/ivoyager_base.cfg
-var _cfg: ConfigFile # res://ivoyager.cfg
-var _autoload_singletons: Array[String] = []
+var _override_cfg: ConfigFile # res://ivoyager.cfg
+var _autoloads := {}
 var _shader_globals := {}
 
 
 func _enter_tree() -> void:
 	_print_plugin_version()
 	_load_base_cfg()
-	_load_or_create_cfg()
-	if !_base_cfg or !_cfg:
+	_load_or_create_override_cfg()
+	if !_base_cfg or !_override_cfg:
 		return
-	_add_autoload_singletons.call_deferred()
+	_add_autoloads.call_deferred()
 	_add_shader_globals.call_deferred()
 
 
 func _exit_tree() -> void:
 	print("Removing I, Voyager - Core (plugin)")
 	_base_cfg = null
-	_cfg = null
+	_override_cfg = null
 	_remove_autoload_singletons()
 	_remove_shader_globals()
 
@@ -69,14 +69,14 @@ func _load_base_cfg() -> void:
 	_base_cfg = null
 
 
-func _load_or_create_cfg() -> void:
-	_cfg = ConfigFile.new()
-	var err := _cfg.load("res://ivoyager.cfg")
+func _load_or_create_override_cfg() -> void:
+	_override_cfg = ConfigFile.new()
+	var err := _override_cfg.load("res://ivoyager.cfg")
 	if err == OK:
 		# Print warning if config sections don't exactly match the template.
 		var template_cfg = ConfigFile.new()
 		template_cfg.load("res://addons/ivoyager_core/ivoyager_template.cfg")
-		if _cfg.get_sections() != template_cfg.get_sections():
+		if _override_cfg.get_sections() != template_cfg.get_sections():
 			print("WARNING: Sections in config file 'res://ivoyager.cfg' do not exactly")
 			print("match the template 'res://addons/ivoyager_core/ivoyager_template.cfg'.")
 			print("This may be due to a core plugin update. In any case, fix your file to match!")
@@ -88,70 +88,56 @@ func _load_or_create_cfg() -> void:
 	err = dir.copy("res://addons/ivoyager_core/ivoyager_template.cfg", "res://ivoyager.cfg")
 	if err != OK:
 		print("ERROR: Failed to copy 'ivoyager.cfg' to the project directory!")
-		_cfg = null
+		_override_cfg = null
 		return
-	err = _cfg.load("res://ivoyager.cfg")
+	err = _override_cfg.load("res://ivoyager.cfg")
 	if err != OK:
 		print("ERROR: Failed to save 'res://ivoyager.cfg'!")
-		_cfg = null
+		_override_cfg = null
 
 
-func _add_autoload_singletons() -> void:
-	var paths: Dictionary = _base_cfg.get_value("autoloads", "paths")
-	var overwrite: Dictionary = _cfg.get_value("autoloads",
-			"merge_overwrite_autoload_paths")
-	paths.merge(overwrite, true)
-	for singleton_name in paths:
-		var path: String = paths[singleton_name]
-		if path:
-			_autoload_singletons.append(singleton_name)
-			add_autoload_singleton(singleton_name, path)
+func _add_autoloads() -> void:
+	for autoload_name in _base_cfg.get_section_keys("autoload"):
+		_autoloads[autoload_name] = _base_cfg.get_value("autoload", autoload_name)
+	if _override_cfg.has_section("autoload_overrides"):
+		for autoload_name in _override_cfg.get_section_keys("autoload_overrides"):
+			var path_or_null: Variant = _override_cfg.get_value("autoload_overrides", autoload_name)
+			if !path_or_null: # "" or null
+				_autoloads.erase(autoload_name)
+				continue
+			_autoloads[autoload_name] = path_or_null
+	for autoload_name in _autoloads:
+		var path: String = _autoloads[autoload_name]
+		add_autoload_singleton(autoload_name, path)
 
 
 func _remove_autoload_singletons() -> void:
-	while _autoload_singletons:
-		var singleton_name: String = _autoload_singletons.pop_back()
-		remove_autoload_singleton(singleton_name)
+	for autoload_name in _autoloads:
+		remove_autoload_singleton(autoload_name)
+	_autoloads.clear()
 
 
 func _add_shader_globals() -> void:
-	for name_ in _base_cfg.get_section_keys("shader_globals"):
-		_shader_globals[name_] = _base_cfg.get_value("shader_globals", name_)
-	if _cfg.has_section("shader_globals"):
-		for name_ in _cfg.get_section_keys("shader_globals"):
-			var dict_or_null: Variant = _cfg.get_value("shader_globals", name_)
+	for global_name in _base_cfg.get_section_keys("shader_globals"):
+		_shader_globals[global_name] = _base_cfg.get_value("shader_globals", global_name)
+	if _override_cfg.has_section("shader_globals_overrides"):
+		for global_name in _override_cfg.get_section_keys("shader_globals_overrides"):
+			var dict_or_null: Variant = _override_cfg.get_value("shader_globals_overrides", global_name)
 			if !dict_or_null: # empty dict or null
-				_shader_globals.erase(name_)
+				_shader_globals.erase(global_name)
 				continue
-			_shader_globals[name_] = dict_or_null
-	if !_shader_globals:
-		return
-	for name_ in _shader_globals:
-		var dict: Dictionary = _shader_globals[name_]
-		
-		prints("Settig: shader_globals/" + "name_", dict)
-		
-		ProjectSettings.set_setting("shader_globals/" + name_, dict)
+			_shader_globals[global_name] = dict_or_null
+	for global_name in _shader_globals:
+		var dict: Dictionary = _shader_globals[global_name]
+		ProjectSettings.set_setting("shader_globals/" + global_name, dict)
 		# These don't show up in editor menu, but are in project.godot and show
-		# up after restart. Does save() do anything...???
-		
-#		ProjectSettings.save()
+		# up after restart.
+	ProjectSettings.save() # Does this do anything...???
 
 
 func _remove_shader_globals() -> void:
-	for name_ in _shader_globals:
-		ProjectSettings.set_setting("shader_globals/" + name_, null)
-#		ProjectSettings.save()
-
-
-
-
-
-
-
-
-
-
-
-
+	for global_name in _shader_globals:
+		ProjectSettings.set_setting("shader_globals/" + global_name, null)
+	ProjectSettings.save() # Does this do anything...???
+	_shader_globals.clear()
 
