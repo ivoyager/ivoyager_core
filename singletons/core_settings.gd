ebd79b674cd406c18d36cc1ff712c6f5588fab10
Charlie Whitfield <charliewhitfield@zoho.com>
2025-03-07 12:10:34 -0500

    Remove unused core setting

--- singletons/core_settings.gd
+++ singletons/core_settings.gd
@@ -30,7 +30,6 @@ extends Node
 var project_name := ""
 var project_version := "" # external project can set for gamesave debuging
 var is_modded := false # this is aspirational
-var save_file_extension := "IVoyagerSave"
 var use_threads := true # false helps for debugging
 var dynamic_orbits := true # allows use of orbit element rates
 var sbg_mag_cutoff_override := INF # overrides small_bodies_group.tsv cutoff if <INF
