5172fc389bdcf7d22087eb5d0c0bcb9b2e3908ba
Charlie Whitfield <charliewhitfield@zoho.com>
2025-10-26 22:01:40 -0400

    Add IVNavButtonBox (deprecate IVBodyHScroll)

--- gui_widgets/nav_button_box.gd
+++ gui_widgets/nav_button_box.gd
@@ -1,4 +1,4 @@
-# body_h_scroll.gd
+# nav_button_box.gd
 # This file is part of I, Voyager
 # https://ivoyager.dev
 # *****************************************************************************
@@ -17,36 +17,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # *****************************************************************************
-class_name IVBodyHScroll
-extends ScrollContainer
+class_name IVNavButtonBox
+extends BoxContainer
 
-# DEPRICATE
 
-## GUI widget.
+## BoxContainer widget for holding [IVNavigationButton] instances
 ##
-## An ancestor Control node must have property [param selection_manager]
-## set to an [IVSelectionManager] before [signal IVGlobal.system_tree_ready].
+## Bodies to be included can be specified by adding one or more table names to
+## [member body_tables]. Alternatively, bodies can be added via [method add_body].
+## [br][br]
 ##
-## Parent GUI should add bodies by calling add methods.
+## The box will expand and fill horizontally and vertically by default. All
+## [IVNavigationButton] instances will be equally sized. In most cases this
+## widget should be inside a ScrollContainer.[br][br]
 ##
-## @depricate
+## Expects property "selection_manager" in an ancestor Control with an
+## IVSelectionManager.
+
+
+# FIXME: Adapt code for possible vertical orientation. Fix resize recursion.
+
 
 const BODYFLAGS_SHOW_IN_NAVIGATION_PANEL := IVBody.BodyFlags.BODYFLAGS_SHOW_IN_NAVIGATION_PANEL
 
 
+@export var body_tables: Array[StringName] = []
+
+
 var _selection_manager: IVSelectionManager
 var _currently_selected: Button
-var _body_tables: Array[String] = []
 var _button_size := 0.0 # scales with widget height
-
-@onready var _mouse_only_gui_nav: bool = IVGlobal.settings[&"mouse_only_gui_nav"]
-@onready var _hbox: HBoxContainer = $HBox
 
 
 
 func _ready() -> void:
 	
-	push_warning("Depricate: Use IVNavButtonBox in ScrollContainer")
+	assert(!vertical, "WIP - Code doesn't suppor vertical yet...")
 	
 	IVGlobal.system_tree_ready.connect(_on_system_tree_ready)
 	IVGlobal.about_to_free_procedural_nodes.connect(_clear_procedural)
@@ -55,36 +61,28 @@ func _ready() -> void:
 		_on_system_tree_ready()
 
 
-
-func add_bodies_from_table(table_name: StringName) -> void:
-	# e.g., 'spacecrafts'
-	if IVGlobal.state.is_system_ready:
-		_add_bodies_from_table(table_name)
-	else:
-		_body_tables.append(table_name)
-
-
 func add_body(body: IVBody) -> void:
 	var button := IVNavigationButton.new(body, 10.0, _selection_manager)
 	button.selected.connect(_on_nav_button_selected.bind(button))
 	button.size_flags_vertical = SIZE_FILL
 	button.custom_minimum_size.x = _button_size # button image grows to fit min x
-	_hbox.add_child(button)
-
+	add_child(button)
 
 
 func _on_system_tree_ready(_dummy := false) -> void:
 	_selection_manager = IVSelectionManager.get_selection_manager(self)
-	for table_name in _body_tables:
+	for table_name in body_tables:
 		_add_bodies_from_table(table_name)
 
 
 func _clear_procedural() -> void:
-	for child in _hbox.get_children():
-		child.queue_free()
+	for child in get_children():
+		var nav_button := child as IVNavigationButton
+		if nav_button:
+			nav_button.queue_free()
 
 
-func _add_bodies_from_table(table_name: String) -> void:
+func _add_bodies_from_table(table_name: StringName) -> void:
 	var table := IVTableData.db_tables[table_name]
 	var body_names: Array = table.name
 	for i in body_names.size():
@@ -96,15 +94,22 @@ func _add_bodies_from_table(table_name: String) -> void:
 
 func _on_nav_button_selected(selected: Button) -> void:
 	_currently_selected = selected
-	if !_mouse_only_gui_nav and !get_viewport().gui_get_focus_owner():
+	if !get_viewport().gui_get_focus_owner():
 		if selected.focus_mode != FOCUS_NONE:
 			selected.grab_focus()
 
 
 func _on_resized() -> void:
+	
+	
 	var button_size := size.y # * 0.7 # 0.7 gives room for scroll bar
+	
+	print("_on_resized", button_size)
+	
 	if _button_size == button_size:
 		return
 	_button_size = button_size
-	for child in _hbox.get_children():
-		(child as IVNavigationButton).custom_minimum_size.x = button_size
+	for child in get_children():
+		var nav_button := child as IVNavigationButton
+		if nav_button:
+			nav_button.custom_minimum_size.x = button_size
