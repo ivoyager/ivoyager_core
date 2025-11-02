9408206d5722a37781b9f612838a1c7b71e2d14a
Charlie Whitfield <charliewhitfield@zoho.com>
2025-11-02 08:19:19 -0500

    IVMainMenuBasePopup can put child Controls in VBoxContainer

--- gui_main/main_menu_base_popup.gd
+++ gui_main/main_menu_base_popup.gd
@@ -20,12 +20,17 @@
 class_name IVMainMenuBasePopup
 extends PopupPanel
 
-## An empty Main Menu popup (base control only) that opens/closes on
+## An empty main menu base popup (without buttons) that opens/closes on
 ## "ui_cancel" action event and "main menu" IVGlobal signals.
 ##
-## This is a base popup upon which you can build a main menu. To build a simple
-## menu, add child MarginContainer with child VBoxContainer, then add to that
-## your menu buttons. You can find many useful "main menu" buttons in
+## All child Controls added to this popup will be added automatically to a
+## VBoxContainer IF AND ONLY IF there is >1 child Control. So developer can
+## construct the menu by simply adding menu buttons (option 1) or can have
+## more control by adding a single top MarginContainer or VBoxContainer or
+## whatever (option 2). For option 1, margins and other style effects can be
+## acheived by editing this node's theme override style box.[br][br]
+##
+## You can find many useful "main menu" buttons in
 ## directory gui_widgets including [IVFullScreenButton], [IVOptionsButton],
 ## [IVHotkeysButton], [IVExitButton], [IVQuitButton] and [IVResumeButton].
 ## Plugin [url=https://github.com/ivoyager/ivoyager_save]I, Voyager - Save[/url]
@@ -33,31 +38,63 @@ extends PopupPanel
 
 const SCENE := "res://addons/ivoyager_core/gui_main/main_menu_base_popup.tscn"
 
-
+## If true (defaut), the menu popup can be opened only after sim start, after
+## the splash screen has been hidden. (It's expected that a game splash screen
+## will have its own menu.)
 @export var sim_started_only := true
+## If true (default), the popup will center itself.
 @export var center := true
+## If true (default), the simulator will stop while the menu is open.
 @export var stop_sim := true
+## If true (default), prevents the popup from closing when the user clicks
+## outside of the menu. They must press escape ("ui_cancel" action) or a
+## "Resume" button to close.
 @export var require_explicit_close := true
+## If true (default), keep popup at minimum size to fit content. This fixes the
+## menu size if the user changes setting "gui_size" from a large value to a
+## smaller value.
+@export var minimal_size := true
+
 
 var _is_explicit_close := false
 
 
 
 func _ready() -> void:
-	exclusive = true # Godot 4.5 seems inconsistent about this
+	exclusive = true
 	hide() # Godot 4.5 editor keeps setting visibility == true !!!
 	IVGlobal.open_main_menu_requested.connect(open)
 	IVGlobal.close_main_menu_requested.connect(close)
 	IVGlobal.close_all_admin_popups_requested.connect(close)
 	IVGlobal.resume_requested.connect(close)
 	popup_hide.connect(_on_popup_hide)
+	
+	# Add to VBoxContainer IF AND ONLY IF >1 Control child...
+	var control_children: Array[Control]
+	for child in get_children():
+		var control := child as Control
+		if control:
+			control_children.append(control)
+	var control_child := control_children[0]
+	if control_children.size() > 1:
+		var vbox := VBoxContainer.new()
+		add_child(vbox)
+		for control in control_children:
+			remove_child(control)
+			vbox.add_child(control)
+		control_child = vbox
+	
+	# Resizing fix...
+	if minimal_size:
+		control_child.minimum_size_changed.connect(_resize)
+
+
 
 
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"ui_cancel"):
 		close()
 		set_input_as_handled()
-
 
 
 func open() -> void:
@@ -79,7 +116,6 @@ func close() -> void:
 	hide()
 
 
-
 func _on_popup_hide() -> void:
 	if require_explicit_close and !_is_explicit_close:
 		show.call_deferred()
@@ -87,3 +123,7 @@ func _on_popup_hide() -> void:
 	_is_explicit_close = false
 	if stop_sim:
 		IVGlobal.sim_run_allowed.emit(self)
+
+
+func _resize() -> void:
+	size = Vector2.ZERO
